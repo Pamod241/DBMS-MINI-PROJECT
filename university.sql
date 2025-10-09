68ea40204be2340147861803a0268c53179ad514
Pamod241 <bhanukapamod2@gmail.com>
2025-10-09 22:12:30 +0530

    create Department Table

--- university.sql
+++ university.sql
@@ -1,1 +1,15 @@
 create database university;
+
+
+use university;
+
+/*Department Table*/
+
+CREATE TABLE Department (
+    Dep_id VARCHAR(10) PRIMARY KEY,
+    D_name VARCHAR(100) NOT NULL
+);
+
+
+
+
