a3969ef1662e4f773da261ae6c5e9c5aad999f29
chathika123 <jalithasamarasinghe2002@gmail.com>
2025-10-09 23:21:55 +0530

    create Quiz Table

--- university.sql
+++ university.sql
@@ -147,3 +147,15 @@ CREATE TABLE Technical_Officer (
     Fullname VARCHAR(100),
     Email VARCHAR(100)
 );
+
+
+/*Quiz Table*/
+
+CREATE TABLE Quiz (
+    Reg_no VARCHAR(15),
+    C_code VARCHAR(10),
+    Mark INT,
+    PRIMARY KEY (Reg_no, C_code),
+    FOREIGN KEY (Reg_no) REFERENCES Student(Reg_no)
+    FOREIGN KEY (C_code) REFERENCES Course(C_code)
+);    
