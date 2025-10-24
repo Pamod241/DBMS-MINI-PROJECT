1a54e5fa008f62f2f437020a6e3a6a5962ff4db2
Geethma-Induwara <megadash1280@gmail.com>
2025-10-24 20:45:22 +0530

    CREATE PROCEDURE for SEMESTER GPA DETAILS FOR GIVEN STUDENT ID

--- procedure.sql
+++ procedure.sql
@@ -12,3 +12,20 @@ DELIMITER ;
 
 
 CALL sem_gpa_details();
+
+
+-- SEMESTER GPA DETAILS FOR GIVEN STUDENT ID
+
+
+DROP PROCEDURE IF EXISTS sem_gpa_details;
+DELIMITER //
+CREATE PROCEDURE sem_gpa_details (IN stu_num VARCHAR(10))
+BEGIN
+ SELECT student_id,SGPA
+ FROM SGPA_check
+ WHERE student_id=stu_num;
+END //
+DELIMITER ;
+
+
+CALL sem_gpa_details('TG0004');
